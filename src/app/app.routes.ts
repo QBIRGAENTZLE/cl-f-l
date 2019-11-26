60cbeb86ab8e7b546fd6d36472f25d6469cdb762
quentin-birgaentzle <quentin.birgaentzle@ext.euronews.com>
2019-11-26 13:25:43 +0100

    fix theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,8 @@ import { Routes } from '@angular/router';
 
 import { StartComponent } from './components/start/start.component';
 import { PeoplePickerComponent } from './components/people-picker/people-picker.component';
-import { ThemePickerComponent } from './components/theme-picker/theme-picker.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', component: StartComponent },
-  { path: 'pick/people', component: PeoplePickerComponent },
-  { path: 'pick/theme', component: ThemePickerComponent },
+  { path: 'pick/people', component: PeoplePickerComponent }
 ];
